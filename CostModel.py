391b7a08eff075c0b1601910bdfdcce9c8a02776
hsbhathiya <hsbathiya@gmail.com>
2015-12-17 13:07:51 +0530

    Cost Model Improvements

--- CostModel.py
+++ CostModel.py
@@ -15,28 +15,29 @@ def violation_precentage(machine_count,machine_unit_power, predicted_arr ):
 
 
 def minToMaxIteration(minVM, maxVM, machine_unit_power,machine_unit_price, predicted_arr, time_gap ):
+    predicted_arr2 = predicted_arr[np.logical_not(np.isnan(predicted_arr))]
     tot_cost = 999999
-    time = time_gap * (float)(len(predicted_arr))/60.0
+    time = time_gap * len(predicted_arr2)/60.0
    # print(time)
     index =   minVM
     violation_selected = 0
     for i in range(minVM, maxVM+1):
-        precentage = violation_precentage(i,machine_unit_power,predicted_arr)
+        precentage = violation_precentage(i,machine_unit_power,predicted_arr2)
         cost =  i*machine_unit_price*time + i*machine_unit_price*time*SLA_func(precentage)
-        #print("VM: %d  Cost: %d, Violation : %d"  %(i, cost, precentage))
+        print("VM: %d  Cost: %d, Violation : %d"  %(i, cost, precentage))
         if cost < tot_cost :
             tot_cost = cost
             index = i
             violation_selected = precentage
         if precentage == 0:
             break
-    return index, precentage
+    return index, violation_selected
 
 def SLA_func(precenage):
     violation_cost = precenage/30.0
     #print(violation_cost)
     return  violation_cost
-
+6
 arr_2d = np.genfromtxt("predict2.csv", delimiter= ",")
 prev = -1 #time of last dataset
 prev_index = 0
@@ -45,6 +46,7 @@ changed = False
 for arr in arr_2d :
     #print(arr[np.logical_not(np.isnan(arr))]) # remove nan
     current = arr[0]
+
     time_gap = 0
 
     if prev != -1:
@@ -55,13 +57,14 @@ for arr in arr_2d :
 
     MIN_VM = 2
     MAX_VM = 15
-    MACHINE_UNIT_POWER = 12
+    MACHINE_UNIT_POWER = 4
     MACHINE_UNIT_PRICE = 120
-    TIME_GAP = 0.2
-    index, violation = minToMaxIteration(MIN_VM,MAX_VM, MACHINE_UNIT_POWER, MACHINE_UNIT_PRICE, arr[1:len(arr)], TIME_GAP)
+    TIME_GAP_IN_PREDICTION = 0.2
+    index, violation = minToMaxIteration(MIN_VM,MAX_VM, MACHINE_UNIT_POWER, MACHINE_UNIT_PRICE, arr[1:len(arr)], TIME_GAP_IN_PREDICTION)
     if index != prev_index:
         changed = True
-    cost  += index * time_gap/60.0 + index * time_gap/60.0* SLA_func(violation)
+    newcost  = index * time_gap/60.0 + index * (time_gap/60.0)* SLA_func(violation)
+    cost += newcost
     prev = current;
     prev_index = index;
-    print("Index: %d, Cost : %.3f" %(prev,cost))
+    print("VM: %d Violation : %.3f Time: %d, newCost: %.3f Cost : %.3f" %(prev_index , violation, prev, newcost,cost))
